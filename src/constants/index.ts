2b6f941570df1fb913ca1f158a23aaa770fd3c32
Tomáš Javorský <8941074+tomasjavorsky@users.noreply.github.com>
2020-04-01 15:09:11 +0200

    fix(endpoint): cors-anywhere

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const endpoint = 'https://cookbook.ack.ee/api/v1';
+export const endpoint = 'https://cors-anywhere.herokuapp.com/https://cookbook.ack.ee/api/v1';
 
 export enum routes {
   home = '/',
